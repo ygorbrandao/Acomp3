600e8eeafaf6da5062f83729fea5a1b950002248
Walber Antonio Ramos Beltrame <1020476@ifes.edu.br>
2019-09-05 11:50:10 -0300

    Gabarito do QuickSort

--- Acomp3/QuickSort.cs
+++ Acomp3/QuickSort.cs
@@ -1,17 +1,30 @@
-﻿using System;
-
-namespace Acomp3
+﻿namespace Acomp3
 {
     class QuickSort : IQuickSort
     {
         public int Part(int[] E, int left, int right)
         {
-            throw new NotImplementedException();
+            int c = E[right];
+            int t, j = left;
+            for (int i = left; i < right; i++)
+            {
+                if (E[i] <= c)
+                {
+                    t = E[j]; E[j] = E[i]; E[i] = t; ++j;
+                }
+            }
+            t = E[j]; E[j] = E[right]; E[right] = t;
+            return j;
         }
 
         public void Sort(int[] E, int left, int right)
         {
-            throw new NotImplementedException();
+            if (left < right)
+            {
+                int q = Part(E, left, right);
+                Sort(E, left, q - 1);
+                Sort(E, q + 1, right);
+            }
         }
 
         public void Sort(int[] E, int n)
--- Acomp3/TailQuickSort.cs
+++ Acomp3/TailQuickSort.cs
@@ -1,12 +1,23 @@
-﻿using System;
-
-namespace Acomp3
+﻿namespace Acomp3
 {
     class TailQuickSort : QuickSort
     {
         public new void Sort(int[] E, int left, int right)
         {
-            throw new NotImplementedException();
+            while (left < right)
+            {
+                int j = Part(E, left, right);
+                if (j - left < right - j)
+                {
+                    Sort(E, left, j - 1);
+                    left = j + 1;
+                }
+                else
+                {
+                    Sort(E, j + 1, right);
+                    right = j - 1;
+                }
+            }
         }
     }
 }

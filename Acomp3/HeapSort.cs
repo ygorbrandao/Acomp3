0df0c660326e389a7a93fffc0e4a19d37f54f0fb
Ygor Brandão Dos Santos <20171ENEL0020@cefetes.br>
2019-09-06 16:58:22 -0300

    Algoritmo Heapsort

--- Acomp3/HeapSort.cs
+++ Acomp3/HeapSort.cs
@@ -4,17 +4,40 @@
     {
         public void Heap(int[] E, int n)
         {
-            throw new System.NotImplementedException();
+            for (int i = n / 2 - 1; i >= 0; i--)
+                Heapify(E, n, i);
+            for (int i = n - 1; i >= 0; i--)
+            {
+                int temp = E[0];
+                E[0] = E[i];
+                E[i] = temp;
+                Heapify(E, i, 0);
+            }
         }
 
         public void Heapify(int[] E, int n, int i)
         {
-            throw new System.NotImplementedException();
+            int largest = i;
+            int left = 2 * i + 1;
+            int right = 2 * i + 2;
+            if (left < n && E[left] > E[largest])
+                largest = left;
+            if (right < n && E[right] > E[largest])
+                largest = right;
+            if (largest != i)
+            {
+                int swap = E[i];
+                E[i] = E[largest];
+                E[largest] = swap;
+                Heapify(E, n, largest);
+            }
         }
 
         public void Sort(int[] E, int n)
         {
-            throw new System.NotImplementedException();
+            Heap(E, n);
         }
     }
 }
+
+
